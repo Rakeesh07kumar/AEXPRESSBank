bd6850d66393333d077f3e8bd0119b238110ac2c
Rakeesh07kumar <rakeshkumar169212@gmail.com>
2023-08-10 17:23:18 +0530

    demo java code

---
+++ Aexpessbank/FactorialDemo.java
@@ -0,0 +1,22 @@
+public class FactorialDemo {
+
+    public static void main(String[] args) {
+        int number = 5;
+        long factorial = calculateFactorial(number);
+        
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+    
+    public static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
+
+
+
+
+
